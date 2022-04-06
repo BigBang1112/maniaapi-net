3b2df3da1a62869eda37ad5ff6dc556ca0ef483b
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-06 19:28:47 +0200

    Add TrackmaniaAPI

---
+++ Src/ManiaAPI.TrackmaniaAPI/TrackmaniaAPI.cs
@@ -0,0 +1,36 @@
+﻿namespace ManiaAPI.TrackmaniaAPI;
+
+public class TrackmaniaAPI
+{
+    public HttpClient Client { get; }
+
+    public TrackmaniaAPI()
+    {
+        Client = new HttpClient
+        {
+            BaseAddress = new Uri("https://api.trackmania.com/")
+        };
+
+        Client.DefaultRequestHeaders.Add("User-Agent", "ManiaAPI.NET (TrackmaniaAPI) by BigBang1112");
+    }
+
+    /// <summary>
+    /// Authorizes with the official API using OAuth2 client credentials.
+    /// </summary>
+    /// <param name="clientId">Client ID.</param>
+    /// <param name="clientSecret">Client secret.</param>
+    public async Task AuthorizeAsync(string clientId, string clientSecret)
+    {
+        var values = new Dictionary<string, string>
+        {
+            { "grant_type", "client_credentials" },
+            { "client_id", clientId },
+            { "client_secret", clientSecret },
+            { "scope", "" }
+        };
+
+        using var response = await Client.PostAsync("api/access_token", new FormUrlEncodedContent(values));
+    
+        var str = await response.Content.ReadAsStringAsync();
+    }
+}
---
+++ Tests/ManiaAPI.TrackmaniaAPI.Tests/Integration/TrackmaniaAPITests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace ManiaAPI.TrackmaniaAPI.Tests.Integration;
+
+public class TrackmaniaAPITests
+{
+    // test authorize
+    [Fact]
+    public async Task RequestManagement()
+    {
+        var tmio = new TrackmaniaAPI();
+        await tmio.AuthorizeAsync("", "");
+    }
+}
