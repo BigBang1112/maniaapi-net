163c55d05719b024824f982880ec6e10002a2dcf
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-10 03:52:00 +0200

    Optimize GetDisplayNamesAsync when no accountIds

--- Src/ManiaAPI.TrackmaniaAPI/ITrackmaniaAPI.cs
+++ Src/ManiaAPI.TrackmaniaAPI/ITrackmaniaAPI.cs
@@ -3,6 +3,6 @@ namespace ManiaAPI.TrackmaniaAPI;
 
 public interface ITrackmaniaAPI
 {
-    Task<Dictionary<Guid, string>> GetDisplayNamesAsync(IEnumerable<Guid> accountIds, CancellationToken cancellationToken = default);
+    ValueTask<Dictionary<Guid, string>> GetDisplayNamesAsync(IEnumerable<Guid> accountIds, CancellationToken cancellationToken = default);
     Task<User> GetUserAsync(CancellationToken cancellationToken = default);
 }
