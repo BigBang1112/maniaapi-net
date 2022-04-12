be0b95c4fe9f6b63173df251f1565e444f2c9f5b
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-12 18:10:25 +0200

    Fix EnsureSuccessStatusCodeAsync to be safer

--- Src/ManiaAPI.Base/Extensions/HttpResponseMessageExtensions.cs
+++ Src/ManiaAPI.Base/Extensions/HttpResponseMessageExtensions.cs
@@ -1,33 +1,39 @@
 ﻿using ManiaAPI.Base;
 using ManiaAPI.Base.Exceptions;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace ManiaAPI.Base.Extensions;
 
 public static class HttpResponseMessageExtensions
 {
     /// <exception cref="ApiRequestException"></exception>
-    public static async ValueTask<HttpResponseMessage> EnsureSuccessStatusCodeAsync<TErrorReponse>(this HttpResponseMessage response, CancellationToken cancellationToken = default)
-        where TErrorReponse : ErrorResponse
+    public static async ValueTask<HttpResponseMessage> EnsureSuccessStatusCodeAsync(this HttpResponseMessage response, CancellationToken cancellationToken = default)
     {
         if (response.IsSuccessStatusCode)
         {
             return response;
         }
 
-        var errorResponse = default(TErrorReponse);
+        var message = default(string);
 
         if (response.Content.Headers.ContentLength > 0)
         {
-            errorResponse = await response.Content.ReadFromJsonAsync<TErrorReponse>(JsonAPI.JsonSerializerOptions, cancellationToken);
+            try
+            {
+                var dictionaryResponse = await response.Content.ReadFromJsonAsync<Dictionary<string, object>>(JsonAPI.JsonSerializerOptions, cancellationToken);
+
+                if (dictionaryResponse is not null && dictionaryResponse.TryGetValue("message", out object? messageObject))
+                {
+                    message = messageObject.ToString();
+                }
+            }
+            catch
+            {
+                // All non-json responses
+            }
         }
 
-        throw new ApiRequestException(response.ReasonPhrase, response.StatusCode, errorResponse);
-    }
-
-    /// <exception cref="ApiRequestException"></exception>
-    public static async ValueTask<HttpResponseMessage> EnsureSuccessStatusCodeAsync(this HttpResponseMessage response, CancellationToken cancellationToken = default)
-    {
-        return await response.EnsureSuccessStatusCodeAsync<ErrorResponse>(cancellationToken);
+        throw new ApiRequestException(message ?? response.ReasonPhrase, response.StatusCode);
     }
 }
