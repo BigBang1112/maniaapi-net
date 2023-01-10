bc66f64e7ea3c9ca3ea71c3208d88b31f536536c
Petr Pivoňka <petrpiv1@gmail.com>
2023-01-10 18:43:24 +0100

    Add GetTopLeaderboardAsync with groupId

--- Src/ManiaAPI.NadeoAPI/INadeoLiveServices.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace ManiaAPI.NadeoAPI;
-
-public interface INadeoLiveServices : INadeoAPI
-{
-    Task<TopLeaderboardCollection> GetTopLeaderboardAsync(string mapUid, int length = 10, int offset = 0, bool onlyWorld = true, CancellationToken cancellationToken = default);
-}
--- Src/ManiaAPI.NadeoAPI/NadeoLiveServices.cs
+++ Src/ManiaAPI.NadeoAPI/NadeoLiveServices.cs
@@ -1,14 +1,25 @@
 ﻿namespace ManiaAPI.NadeoAPI;
 
+public interface INadeoLiveServices : INadeoAPI
+{
+    Task<TopLeaderboardCollection> GetTopLeaderboardAsync(string mapUid, int length = 10, int offset = 0, bool onlyWorld = true, CancellationToken cancellationToken = default);
+    Task<TopLeaderboardCollection> GetTopLeaderboardAsync(string mapUid, string groupId, int length = 10, int offset = 0, bool onlyWorld = true, CancellationToken cancellationToken = default);
+}
+
 public class NadeoLiveServices : NadeoAPI, INadeoLiveServices
 {
     public NadeoLiveServices(bool automaticallyAuthorize = true) : base("https://live-services.trackmania.nadeo.live/api/", automaticallyAuthorize)
     {
-
+        
     }
 
     public async Task<TopLeaderboardCollection> GetTopLeaderboardAsync(string mapUid, int length = 10, int offset = 0, bool onlyWorld = true, CancellationToken cancellationToken = default)
     {
         return await GetApiAsync<TopLeaderboardCollection>($"token/leaderboard/group/Personal_Best/map/{mapUid}/top?length={length}&offset={offset}&onlyWorld={onlyWorld}", cancellationToken);
+    }
+
+    public async Task<TopLeaderboardCollection> GetTopLeaderboardAsync(string mapUid, string groupId, int length = 10, int offset = 0, bool onlyWorld = true, CancellationToken cancellationToken = default)
+    {
+        return await GetApiAsync<TopLeaderboardCollection>($"token/leaderboard/group/{groupId}/map/{mapUid}/top?length={length}&offset={offset}&onlyWorld={onlyWorld}", cancellationToken);
     }
 }
