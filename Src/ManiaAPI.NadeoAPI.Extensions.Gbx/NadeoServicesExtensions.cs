529f703dac424c444bca5bcccf26fbfd6d107f74
Petr Pivoňka <petrpiv1@gmail.com>
2024-06-14 04:14:07 +0200

    Update packages

--- Src/ManiaAPI.NadeoAPI.Extensions.Gbx/NadeoServicesExtensions.cs
+++ Src/ManiaAPI.NadeoAPI.Extensions.Gbx/NadeoServicesExtensions.cs
@@ -1,5 +1,4 @@
-﻿using GBX.NET;
-using GBX.NET.Engines.Game;
+﻿using GBX.NET.Engines.Game;
 using ManiaAPI.NadeoAPI.JsonContexts;
 using System.Net.Http.Json;
 using TmEssentials;
@@ -64,18 +63,18 @@ public static class NadeoServicesExtensions
 
     private static MultipartFormDataContent CreateContent(BufferedStream bufferedStream, string fileName)
     {
-        var map = GameBox.ParseNodeHeader<CGameCtnChallenge>(bufferedStream);
+        var map = GBX.NET.Gbx.ParseHeaderNode<CGameCtnChallenge>(bufferedStream);
 
         bufferedStream.Position = 0;
 
         return new MultipartFormDataContent
         {
-            { new StringContent(map.TMObjective_AuthorTime.GetValueOrDefault().TotalMilliseconds.ToString()), "authorScore" },
-            { new StringContent(map.TMObjective_GoldTime.GetValueOrDefault().TotalMilliseconds.ToString()), "goldScore" },
-            { new StringContent(map.TMObjective_SilverTime.GetValueOrDefault().TotalMilliseconds.ToString()), "silverScore" },
-            { new StringContent(map.TMObjective_BronzeTime.GetValueOrDefault().TotalMilliseconds.ToString()), "bronzeScore" },
+            { new StringContent(map.AuthorTime.GetValueOrDefault().TotalMilliseconds.ToString()), "authorScore" },
+            { new StringContent(map.GoldTime.GetValueOrDefault().TotalMilliseconds.ToString()), "goldScore" },
+            { new StringContent(map.SilverTime.GetValueOrDefault().TotalMilliseconds.ToString()), "silverScore" },
+            { new StringContent(map.BronzeTime.GetValueOrDefault().TotalMilliseconds.ToString()), "bronzeScore" },
             { new StringContent(AccountUtils.ToAccountId(map.AuthorLogin).ToString()), "author" },
-            { new StringContent(map.Collection == 26 ? "Stadium" : map.Collection), "collectionName" },
+            { new StringContent(map.Collection.HasValue && map.Collection.Value.Number != 26 ? map.Collection : "Stadium"), "collectionName" },
             { new StringContent(map.MapStyle ?? string.Empty), "mapStyle" },
             { new StringContent(map.MapType ?? string.Empty), "mapType" },
             { new StringContent(map.MapUid), "mapUid" },
