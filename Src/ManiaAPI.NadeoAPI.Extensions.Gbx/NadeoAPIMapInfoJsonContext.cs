73b51eb7a8429900f18b1a67dcb373c03b3467da
Petr Pivoňka <petrpiv1@gmail.com>
2024-12-21 22:03:34 +0100

    Remove unused stuff

--- Src/ManiaAPI.NadeoAPI.Extensions.Gbx/NadeoAPIMapInfoJsonContext.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace ManiaAPI.NadeoAPI.Extensions.Gbx;
-
-[JsonSerializable(typeof(MapInfoSubmit))]
-[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
-internal sealed partial class NadeoAPIMapInfoJsonContext : JsonSerializerContext;
