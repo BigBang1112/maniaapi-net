5b0ab9312408be7991113493dbe393562fa2e79a
Petr Pivoňka <petrpiv1@gmail.com>
2024-12-21 20:04:43 +0100

    Add more overloads

--- Src/ManiaAPI.NadeoAPI.Extensions.Gbx/NadeoServicesExtensions.cs
+++ Src/ManiaAPI.NadeoAPI.Extensions.Gbx/NadeoServicesExtensions.cs
@@ -39,6 +39,37 @@ public static class NadeoServicesExtensions
     /// Does not work with <see cref="AuthorizationMethod.DedicatedServer"/>.
     /// </summary>
     /// <param name="services"></param>
+    /// <param name="stream"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="FormatException">Author login is invalid.</exception>
+    /// <exception cref="NadeoAPIResponseException"></exception>
+    public static async Task<MapInfo> UploadMapAsync(this INadeoServices services, FileStream stream, CancellationToken cancellationToken = default)
+    {
+        return await UploadMapAsync(services, stream, Path.GetFileName(stream.Name), cancellationToken);
+    }
+
+    /// <summary>
+    /// Does not work with <see cref="AuthorizationMethod.DedicatedServer"/>.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="filePath"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="FormatException">Author login is invalid.</exception>
+    /// <exception cref="NadeoAPIResponseException"></exception>
+    public static async Task<MapInfo> UploadMapAsync(this INadeoServices services, string filePath, CancellationToken cancellationToken = default)
+    {
+        await using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.Asynchronous);
+        return await UploadMapAsync(services, stream, Path.GetFileName(filePath), cancellationToken);
+    }
+
+    /// <summary>
+    /// Does not work with <see cref="AuthorizationMethod.DedicatedServer"/>.
+    /// </summary>
+    /// <param name="services"></param>
     /// <param name="mapId"></param>
     /// <param name="stream"></param>
     /// <param name="fileName"></param>
@@ -59,6 +90,39 @@ public static class NadeoServicesExtensions
         using var response = await services.SendAsync(HttpMethod.Post, $"maps/{mapId}", content, cancellationToken);
 
         return await response.Content.ReadFromJsonAsync(NadeoAPIJsonContext.Default.MapInfo, cancellationToken) ?? throw new Exception("This shouldn't be null.");
+    }
+
+    /// <summary>
+    /// Does not work with <see cref="AuthorizationMethod.DedicatedServer"/>.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="mapId"></param>
+    /// <param name="stream"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="FormatException">Author login is invalid.</exception>
+    /// <exception cref="NadeoAPIResponseException"></exception>
+    public static async Task<MapInfo> UpdateMapAsync(this INadeoServices services, Guid mapId, FileStream stream, CancellationToken cancellationToken = default)
+    {
+        return await UpdateMapAsync(services, mapId, stream, Path.GetFileName(stream.Name), cancellationToken);
+    }
+
+    /// <summary>
+    /// Does not work with <see cref="AuthorizationMethod.DedicatedServer"/>.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="mapId"></param>
+    /// <param name="filePath"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="FormatException">Author login is invalid.</exception>
+    /// <exception cref="NadeoAPIResponseException"></exception>
+    public static async Task<MapInfo> UpdateMapAsync(this INadeoServices services, Guid mapId, string filePath, CancellationToken cancellationToken = default)
+    {
+        await using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.Asynchronous);
+        return await UpdateMapAsync(services, mapId, stream, Path.GetFileName(filePath), cancellationToken);
     }
 
     private static MultipartFormDataContent CreateContent(BufferedStream bufferedStream, string fileName)
