1d9649cfa30bb011a1d0aa876e76a76dd5c97504
Petr Pivoňka <petrpiv1@gmail.com>
2025-01-16 02:37:02 +0100

    Add GetCampaignLeaderBoard (messy)

--- Src/ManiaAPI.XmlRpc/MP4/MasterServerMP4.cs
+++ Src/ManiaAPI.XmlRpc/MP4/MasterServerMP4.cs
@@ -1,18 +1,12 @@
 ﻿using MinimalXmlReader;
+using System.Diagnostics;
 using System.Text;
 
 namespace ManiaAPI.XmlRpc.MP4;
 
 public class MasterServerMP4 : MasterServer
 {
-    protected async Task<string> SendAsync(StringContent content, CancellationToken cancellationToken)
-    {
-        using var response = await Client.PostAsync("http://relay02.v04.maniaplanet.com/game/request.php", content, cancellationToken);
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsStringAsync(cancellationToken);
-    }
-
-    public async Task<IEnumerable<LeaderboardItem>> GetMapLeaderBoardAsync(string titleId, string mapUid, int count = 10, int offset = 0, string zone = "World", string context = "", CancellationToken cancellationToken = default)
+    protected async Task<string> SendAsync(string titleId, string requestName, string parameters, CancellationToken cancellationToken)
     {
         var content = new StringContent(@$"
 <root>
@@ -22,27 +16,171 @@ public class MasterServerMP4 : MasterServer
         <title>{titleId}</title>
     </game>
     <request>
-        <name>GetMapLeaderBoard</name>
-        <params>
+        <name>{requestName}</name>
+        <params>{parameters}</params>
+    </request>
+</root>", Encoding.UTF8, "text/xml");
+
+        using var response = await Client.PostAsync("http://relay02.v04.maniaplanet.com/game/request.php", content, cancellationToken);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<LeaderboardItem>> GetCampaignLeaderBoardAsync(
+        string titleId,
+        int count = 10,
+        int offset = 0,
+        string zone = "World",
+        CancellationToken cancellationToken = default)
+    {
+        const string RequestName = "GetCampaignLeaderBoard";
+        var responseStr = await SendAsync(titleId, RequestName, @$"
+            <f>{offset}</f>
+            <n>{count}</n>
+            <c>{titleId}</c>
+            <m></m>
+            <t></t>
+            <z>{zone}</z>
+            <s>SkillPoint</s>", cancellationToken);
+        var xml = new MiniXmlReader(responseStr);
+        xml.SkipProcessingInstruction();
+        if (!MemoryExtensions.Equals(xml.ReadStartElement(), "r", StringComparison.OrdinalIgnoreCase))
+        {
+            throw new Exception("<r> (request) not found");
+        }
+
+        var items = new List<LeaderboardItem>();
+
+        while (xml.TryReadStartElement(out var responseElement))
+        {
+            switch (responseElement)
+            {
+                case "r":
+                    while (xml.TryReadStartElement(out var contentElement))
+                    {
+                        switch (contentElement)
+                        {
+                            case "n":
+                                var requestName = xml.ReadContentAsString();
+                                if (requestName != RequestName)
+                                {
+                                    //throw new Exception("Invalid response"); invalid xml will respond with empty request name
+                                }
+                                break;
+                            case "c":
+                                while (xml.TryReadStartElement("i"))
+                                {
+                                    var rank = 0;
+                                    var login = string.Empty;
+                                    var nickname = string.Empty;
+                                    var score = 0u;
+                                    var fileName = string.Empty;
+                                    var downloadUrl = string.Empty;
+
+                                    while (xml.TryReadStartElement(out var itemElement))
+                                    {
+                                        switch (itemElement)
+                                        {
+                                            case "r":
+                                                rank = int.Parse(xml.ReadContent());
+                                                break;
+                                            case "l":
+                                                login = xml.ReadContentAsString();
+                                                break;
+                                            case "n":
+                                                nickname = xml.ReadContentAsString();
+                                                break;
+                                            case "s":
+                                                score = uint.Parse(xml.ReadContent());
+                                                break;
+                                            case "f":
+                                                fileName = xml.ReadContentAsString();
+                                                break;
+                                            case "u":
+                                                downloadUrl = xml.ReadContentAsString();
+                                                break;
+                                            default:
+                                                xml.ReadContent();
+                                                break;
+                                        }
+
+                                        Debug.Assert(xml.SkipEndElement());
+                                    }
+
+                                    items.Add(new LeaderboardItem(rank, login, nickname, score, fileName, downloadUrl));
+
+                                    Debug.Assert(xml.SkipEndElement()); // i
+                                }
+                                break;
+                            case "e":
+                                var value = 0;
+                                var message = string.Empty;
+
+                                while (xml.TryReadStartElement(out var errorElement))
+                                {
+                                    switch (errorElement)
+                                    {
+                                        case "v":
+                                            value = int.Parse(xml.ReadContent());
+                                            break;
+                                        case "m":
+                                            message = xml.ReadContentAsString();
+                                            break;
+                                        default:
+                                            xml.ReadContent();
+                                            break;
+                                    }
+
+                                    Debug.Assert(xml.SkipEndElement());
+                                }
+
+                                throw new Exception($"Error {value}: {message}");
+                        }
+
+                        Debug.Assert(xml.SkipEndElement()); // n, c or e
+                    }
+
+                    break;
+                case "e":
+                    var executionTime = xml.ReadContentAsString();
+                    break;
+            }
+
+            Debug.Assert(xml.SkipEndElement());
+        }
+
+        return items;
+    }
+
+    public async Task<IEnumerable<LeaderboardItem>> GetMapLeaderBoardAsync(
+        string titleId, 
+        string mapUid, 
+        int count = 10, 
+        int offset = 0, 
+        string zone = "World",
+        string context = "",
+        CancellationToken cancellationToken = default)
+    {
+        const string RequestName = "GetMapLeaderBoard";
+
+        var responseStr = await SendAsync(titleId, RequestName, @$"
             <m>{mapUid}</m>
             <n>{count}</n>
             <f>{offset}</f>
             <z>{zone}</z>
             <c></c>
             <t></t>
-            <s>MapRecord</s>
-        </params>
-    </request>
-</root>", Encoding.UTF8, "text/xml");
-        var responseStr = await SendAsync(content, cancellationToken);
+            <s>MapRecord</s>", cancellationToken);
+
         var xml = new MiniXmlReader(responseStr);
         xml.SkipProcessingInstruction();
+
         var requestElement = xml.ReadStartElement();
         var responseElement = xml.ReadStartElement();
         var requestNameElement = xml.ReadStartElement();
         var requestName = xml.ReadContentAsString();
 
-        if (requestName != "GetMapLeaderBoard")
+        if (requestName != RequestName)
         {
             throw new Exception("Invalid response");
         }
@@ -52,7 +190,7 @@ public class MasterServerMP4 : MasterServer
 
         var items = new List<LeaderboardItem>();
 
-        while (xml.ReadStartElement("i", out _))
+        while (xml.TryReadStartElement("i"))
         {
             var rankElement = xml.ReadStartElement();
             var rank = int.Parse(xml.ReadContent());
