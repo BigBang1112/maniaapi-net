0a250086e4574d20ea7427f35413cc5c67f75ca3
Petr Pivoňka <petrpiv1@gmail.com>
2025-08-24 00:00:59 +0200

    WS: return httpclientbuilder

--- Src/ManiaAPI.TrackmaniaWS.Extensions.Hosting/TrackmaniaWSServiceExtensions.cs
+++ Src/ManiaAPI.TrackmaniaWS.Extensions.Hosting/TrackmaniaWSServiceExtensions.cs
@@ -4,11 +4,12 @@ namespace ManiaAPI.TrackmaniaWS.Extensions.Hosting;
 
 public static class TrackmaniaWSServiceExtensions
 {
-    public static void AddTrackmaniaWS(this IServiceCollection services, TrackmaniaWSOptions options)
+    public static IHttpClientBuilder AddTrackmaniaWS(this IServiceCollection services, TrackmaniaWSOptions options)
     {
-        services.AddHttpClient<TrackmaniaWS>();
+        var builder = services.AddHttpClient<TrackmaniaWS>();
         services.AddTransient(provider => new TrackmaniaWS(
             options.Credentials ?? throw new Exception("Credentials are required currently."),
             provider.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(TrackmaniaWS))));
+        return builder;
     }
 }
