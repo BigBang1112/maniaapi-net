ca692f223a1c3a030b05438774b6cc964fc2dde0
Petr Pivoňka <petrpiv1@gmail.com>
2025-10-11 23:19:46 +0200

    Add reauthorize after 20 hours

--- Src/ManiaAPI.NadeoAPI/NadeoAPIHandler.cs
+++ Src/ManiaAPI.NadeoAPI/NadeoAPIHandler.cs
@@ -9,6 +9,11 @@ public sealed class NadeoAPIHandler
     /// </summary>
     public NadeoAPICredentials? PendingCredentials { get; set; }
 
+    /// <summary>
+    /// Credentials used for maintaining authentication after refresh token expires.
+    /// </summary>
+    public NadeoAPICredentials? SavedCredentials { get; set; }
+
     internal AuthenticationHeaderValue? Authorization { get; set; }
 
     internal string? RefreshToken { get; set; }
