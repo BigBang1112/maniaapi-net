2dd902d4cac65b319950d556ed772b42d17eceb7
Kem <legacinskas@gmail.com>
2026-03-04 00:33:00 +0200

    Add Timestamp property to leaderboard Record type

--- Src/ManiaAPI.NadeoAPI/Record.cs
+++ Src/ManiaAPI.NadeoAPI/Record.cs
@@ -1,10 +1,11 @@
 ﻿using System.Text.Json.Serialization;
+using ManiaAPI.NadeoAPI.Converters;
 using TmEssentials;
 using TmEssentials.Converters;
 
 namespace ManiaAPI.NadeoAPI;
 
-public sealed record Record(Guid AccountId, Guid ZoneId, string ZoneName, int Position, [property: JsonConverter(typeof(JsonTimeInt32Converter))] TimeInt32 Score)
+public sealed record Record(Guid AccountId, Guid ZoneId, string ZoneName, int Position, [property: JsonConverter(typeof(JsonTimeInt32Converter))] TimeInt32 Score, [property: JsonConverter(typeof(DateTimeOffsetUnixConverter))] DateTimeOffset Timestamp)
 {
     public override string ToString()
     {
