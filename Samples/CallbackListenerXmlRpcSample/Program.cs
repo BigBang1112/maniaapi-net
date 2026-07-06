7b3c88dd3dd1a2c66f9e1485d162f751e919f190
Petr Pivoňka <petrpiv1@gmail.com>
2026-07-06 18:46:36 +0200

    Add CallbackListenerXmlRpcSample

---
+++ Samples/CallbackListenerXmlRpcSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using ManiaAPI.XmlRpc;
+using Microsoft.Extensions.Logging;
+
+var logger = LoggerFactory.Create(builder =>
+{
+    builder.AddSimpleConsole(options =>
+    {
+        options.IncludeScopes = true;
+        options.SingleLine = true;
+    });
+    builder.SetMinimumLevel(LogLevel.Trace);
+}).CreateLogger<XmlRpcClient>();
+
+using var xmlRpc = await XmlRpcClient.ConnectAsync("127.0.0.1", logger: logger);
+
+await xmlRpc.CallAsync("Authenticate", "SuperAdmin", "SuperAdmin");
+await xmlRpc.CallAsync("EnableCallbacks", true);
+
+await foreach (var callback in xmlRpc.StreamCallbacksAsync())
+{
+    Console.WriteLine($"{callback.MethodName}: {string.Join(", ", callback.MethodParams)}");
+}
